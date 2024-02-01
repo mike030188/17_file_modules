30bd40db2a35894a28d72e9bef438c3e2c3d9e5e
mike030188 <halol.imnida2023@gmail.com>
2024-02-01 16:58:02 +0500

    feat: calculation process is working

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Salom dunyo!");
+// Module Packege -> FILE
+const calculate = require("./hisob.js");
+
+const natija = calculate.kopaytirish(50, 30);
+console.log("Natija:", natija);
+
+
+console.log("================================");
+
+const natija2 = calculate.qoshish(77, 33);
+console.log("Natija:", natija2);
